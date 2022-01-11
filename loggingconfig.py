1c66da213d3604b68f2e53007e6fef59f9e121e2
Joe Freeman <joe@freemansoft.com>
2022-01-11 08:15:57 -0500

    demo DEBUG level behavior

--- loggingconfig.py
+++ loggingconfig.py
@@ -8,6 +8,7 @@ def load_logging():
         config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
     logger = logging.getLogger(__name__)
+    # Demonstration code. This logs the handlers just for this logger
     logger.debug("handlers: %s", logger.handlers)
     if len(logger.handlers) == 0:
         logger.warn("no handlers found for this logger")
