57e2c7561b264153394f0d2055d73d07f1d8d638
Joe Freeman <joe@freemansoft.com>
2022-01-11 19:18:13 -0500

    logger clean up for video

--- loggingconfig.py
+++ loggingconfig.py
@@ -2,13 +2,8 @@ import logging
 import logging.config
 import yaml
 
-
+# TODO add error handling or debug output
 def load_logging():
     with open("logging_config.yaml", "r") as f:
         config = yaml.safe_load(f.read())
         logging.config.dictConfig(config)
-    logger = logging.getLogger(__name__)
-    # Demonstration code. This logs the handlers just for this logger
-    logger.debug("handlers: %s", logger.handlers)
-    if len(logger.handlers) == 0:
-        logger.warn("no handlers found for this logger")
