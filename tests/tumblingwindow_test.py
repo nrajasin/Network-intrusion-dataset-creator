d63dfcb91e1b09f86765c2c68f29688ea45ffdb8
Joe Freeman <joe@freemansoft.com>
2022-11-05 17:21:18 -0400

    some variablization

--- tests/tumblingwindow_test.py
+++ tests/tumblingwindow_test.py
@@ -1,6 +1,6 @@
-# unit test for tumbling window
-#
-# TODO: write tests for calculate_tumbling_window()
+# unit test for tumblingwindow.py
+# pytest all test methods are test_*() or *_test()
+# __init__.py tells pytest this isn't the root
 
 from unittest import TestCase
 from tumblingwindow import TumblingWindow
@@ -21,28 +21,37 @@ window_start_time_minus_sec_1 = window_start_time - 1000
 window_start_time_plus_sec_1 = window_start_time + 1000
 window_start_time_plus_sec_5 = window_start_time + 5000
 # windows is 5000msec or 10 packets
-tw = TumblingWindow(5000, 10)
+window_size_msec = 5000
+packet_num_max = 10
+tw = TumblingWindow(window_size_msec, packet_num_max)
 
-
+# 1000 msec is less that 5000 msec window
 def test_inside_window():
     assert not tw.is_past_current_window(
         window_start_time_plus_sec_1, window_start_time, 1
     )
 
 
+# 60000 msec is greater than 5000 msec
 def test_time_past_window():
     assert tw.is_past_current_window(window_start_time_plus_min_1, window_start_time, 1)
 
 
+# 8+1 packets is < max packets of 10
 def test_another_packet_ok():
     assert not (
-        tw.is_past_current_window(window_start_time_plus_sec_1, window_start_time, 8)
+        tw.is_past_current_window(
+            window_start_time_plus_sec_1, window_start_time, packet_num_max - 2
+        )
     )
 
 
+# 10+1 packets is > 10 packets
 def test_one_packet_too_many():
     assert (
-        tw.is_past_current_window(window_start_time_minus_sec_1, window_start_time, 10)
+        tw.is_past_current_window(
+            window_start_time_minus_sec_1, window_start_time, packet_num_max
+        )
     ) == True
 
 
@@ -56,19 +65,25 @@ def test_time_before_start_window():
 
 def test_same_tumbling_window():
     assert not tw.is_past_current_window(
-        window_start_time_plus_sec_1, window_start_time, 2
+        window_start_time_plus_sec_1, window_start_time, packet_num_max / 2
     )
 
     assert (
-        tw.calculate_tumbling_window(window_start_time_plus_sec_1, window_start_time, 2)
+        tw.calculate_tumbling_window(
+            window_start_time_plus_sec_1, window_start_time, packet_num_max / 2
+        )
         == window_start_time
     )
 
 
 def test_next_tumbling_window():
-    assert tw.is_past_current_window(window_start_time_plus_sec_5, window_start_time, 2)
+    assert tw.is_past_current_window(
+        window_start_time_plus_sec_5, window_start_time, packet_num_max / 2
+    )
     # should go to the next window
     assert (
-        tw.calculate_tumbling_window(window_start_time_plus_sec_5, window_start_time, 2)
-        == window_start_time + 5000
+        tw.calculate_tumbling_window(
+            window_start_time_plus_sec_5, window_start_time, packet_num_max / 2
+        )
+        == window_start_time + window_size_msec
     )
